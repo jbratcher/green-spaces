8f1eb62c1561be18ec9895175e5f7a7755fa14bb
jbratcher <jeremybratcher@gmail.com>
2019-10-29 16:53:25 -0400

    intial commit, user, role, and spaces tables

---
+++ app/Models/Role.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Role extends Model {
+
+  user() {
+    return this.belongsToMany('App/Models/User')
+  }
+
+}
+
+module.exports = Role
