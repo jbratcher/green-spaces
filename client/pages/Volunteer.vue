9c6b5b61016fa682cbb2b5725ff8887a18cd756e
jbratcher <jeremybratcher@gmail.com>
2019-11-12 22:10:43 -0500

    wip: add event form for logged in users

--- client/pages/Volunteer.vue
+++ client/pages/Volunteer.vue
@@ -6,12 +6,12 @@
         <h1 class="display-3 pt-6 pb-3">
           Give your time
         </h1>
-        <p class="headline pt=3 pb-6">
+        <p class="headline pt-3 pb-6">
           Your time helps fight climate change
         </p>
       </v-sheet>
 
-      <v-sheet>
+      <v-sheet class="py-10">
         <h2 class="display-1 py-5">
           Upcoming Events
         </h2>
@@ -27,19 +27,96 @@
         </v-sheet>
       </v-sheet>
 
+      <v-sheet
+        v-if="$auth.loggedIn"
+        class="py-10"
+      >
+        <h3>Add a new event</h3>
+        <v-sheet class="mx-auto my-10" width="60vw">
+          <form>
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              :counter="30"
+              label="Name"
+              required
+              @input="$v.name.$touch()"
+              @blur="$v.name.$touch()"
+            />
+            <v-text-field
+              v-model="start"
+              :error-messages="startErrors"
+              label="Date"
+              required
+              @input="$v.start.$touch()"
+              @blur="$v.start.$touch()"
+            />
+            <v-text-field
+              v-model="description"
+              :error-messages="descriptionErrors"
+              label="Description"
+              required
+              @input="$v.description.$touch()"
+              @blur="$v.description.$touch()"
+            />
+
+            <v-btn class="mr-4" @click="submit">
+              submit
+            </v-btn>
+            <v-btn @click="clear">
+              clear
+            </v-btn>
+          </form>
+        </v-sheet>
+      </v-sheet>
+
     </v-col>
   </v-layout>
 </template>
 
 <script>
 import { mapActions, mapMutations, mapState } from 'vuex';
+import { validationMixin } from 'vuelidate';
+import { required, maxLength } from 'vuelidate/lib/validators';
 
 export default {
+  mixins: [validationMixin],
+  validations: {
+    name: { required, maxLength: maxLength(30) },
+    start: { required, String },
+    description: { required, String },
+  },
+  data: () => ({
+    name: '',
+    start: '',
+    description: '',
+  }),
   computed: {
     ...mapState('spaceEvents', [
       'newSpaceEventName',
       'spaceEvents',
     ]),
+    nameErrors () {
+      const errors = []
+      if (!this.$v.name.$dirty) { return errors }
+      !this.$v.name.maxLength && errors.push('Name must be at most 30 characters long')
+      !this.$v.name.required && errors.push('Name is required.')
+      return errors
+    },
+    startErrors () {
+      const errors = []
+      if (!this.$v.start.$dirty) { return errors }
+      !this.$v.start.start && errors.push('Must be valid date')
+      !this.$v.start.required && errors.push('Date is required')
+      return errors
+    },
+    descriptionErrors () {
+      const errors = []
+      if (!this.$v.description.$dirty) { return errors }
+      !this.$v.description.description && errors.push('Must enter a description')
+      !this.$v.description.required && errors.push('Description is required')
+      return errors
+    },
   },
   created () {
     this.fetchSpaceEvents();
@@ -53,6 +130,15 @@ export default {
       'createSpaceEvent',
       'fetchSpaceEvents',
     ]),
+    submit () {
+      this.$v.$touch()
+    },
+    clear () {
+      this.$v.$reset()
+      this.name = ''
+      this.start = ''
+      this.description = ''
+    },
   }
 }
 </script>
